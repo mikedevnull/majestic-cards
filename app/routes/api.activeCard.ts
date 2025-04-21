e5d4cb66ad4fe78da59cd6cc394562de316fe91f
Michael Meier <michael-meier@bluesheep.de>
2025-04-21 21:43:23 +0200

    feat: add api endpoint to change the current active card
    does not provide any real functionality, just a hook to trigger a change to the active card

---
+++ app/routes/api.activeCard.ts
@@ -0,0 +1,26 @@
+import { ActionFunctionArgs } from "@remix-run/node";
+import { z } from "zod";
+import {
+  cardChangeTriggerSchema,
+  changeActiveCard,
+} from "~/lib/activeCard.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  try {
+    switch (request.method) {
+      case "PUT": {
+        const raw = await request.json();
+        const data = cardChangeTriggerSchema.parse(raw);
+        changeActiveCard(data);
+        return new Response("Card changed", { status: 200 });
+      }
+    }
+    return new Response("Bad request", { status: 400 });
+  } catch (error) {
+    if (error instanceof z.ZodError || error instanceof SyntaxError) {
+      return new Response("Bad data", { status: 400 });
+    }
+
+    return new Response("Server error", { status: 500 });
+  }
+};
