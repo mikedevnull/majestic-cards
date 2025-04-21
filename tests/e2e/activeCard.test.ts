c1fc00e75d0366661976c1cf97ef675c53a30965
Michael Meier <michael-meier@bluesheep.de>
2025-04-21 21:49:08 +0200

    test: add first simple test to trigger a card change

--- tests/e2e/activeCard.test.ts
+++ tests/e2e/activeCard.test.ts
@@ -5,3 +5,16 @@ test("Main page shows no active card if non present", async ({ page }) => {
 
   await expect(page.getByText("No active card in reader")).toBeVisible();
 });
+
+test("Main page shows unknown active card if change has been triggered", async ({
+  page,
+  request,
+}) => {
+  const changeCard = await request.put("/api/activeCard", {
+    data: { id: "some-id-123" },
+  });
+  expect(changeCard.ok()).toBeTruthy();
+  await page.goto("/");
+
+  await expect(page.getByText("Unknown card in reader")).toBeVisible();
+});
