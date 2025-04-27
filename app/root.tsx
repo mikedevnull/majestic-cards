8144343dd48799b3151e5e8a3302d60282e28013
Michael Meier <michael-meier@bluesheep.de>
2025-04-27 19:26:42 +0200

    chore: restructure css

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  isRouteErrorResponse,
   Links,
   Meta,
   NavLink,
@@ -7,11 +8,35 @@ import {
   ScrollRestoration,
 } from "react-router";
 import type { LinksFunction } from "react-router";
+import { Route } from "./+types/root";
 
 import "normalize.css";
 import "./app.css";
 
 export const links: LinksFunction = () => [];
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
