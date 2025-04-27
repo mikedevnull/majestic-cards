64ca542e7f14c30f8d1da98efc2f32956ccd7609
Michael Meier <michael-meier@bluesheep.de>
2025-04-27 17:21:37 +0200

    feat: add optional "action" button to card

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,13 +1,19 @@
 import cardStyles from "./cards.module.css";
 import noImage from "~/assets/file-64.png";
 
+type CardAction = {
+  title: string;
+  onClick: () => void;
+};
+
 type CardPros = {
   active: boolean;
   title: string;
   image?: React.ReactNode;
+  action?: CardAction;
 };
 
-export function Card({ active, title, image }: CardPros) {
+export function Card({ active, title, image, action }: CardPros) {
   const activeClass = active ? "" : cardStyles.cardInactive;
   if (active && !image) {
     image = (
@@ -15,10 +21,16 @@ export function Card({ active, title, image }: CardPros) {
       <img src={noImage} alt="No image" />
     );
   }
+  const actions = action ? (
+    <div className={cardStyles.actions}>
+      <button onClick={() => action.onClick()}>{action.title}</button>
+    </div>
+  ) : null;
   return (
     <div className={`${cardStyles.card} ` + activeClass}>
       <div className={cardStyles.cardImage}>{image}</div>
       <span>{title}</span>
+      {actions}
     </div>
   );
 }
