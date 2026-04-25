f19735fb28ff4d0be4efbf6680a26aa8fa8eb0bc
Michael Meier <michael-meier@bluesheep.de>
2026-04-25 10:27:32 +0200

    initial commit with plain nextjs

--- app/routes/api.lyrion.players.ts
+++
@@ -1,5 +0,0 @@
-import { serverApi } from "~/lib/lyrion.server";
-
-export const loader = async () => {
-  return await serverApi.getPlayers();
-};
--- app/routes/library.tsx
+++
@@ -1,5 +0,0 @@
-import cardStyles from "~/components/cards.module.css";
-
-export default function Library() {
-  return <div className={cardStyles.cardGrid}></div>;
-}
