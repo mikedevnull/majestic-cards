70ced046b2691f9c55db191b3a44b5cf5f182bef
Michael Meier <michael-meier@bluesheep.de>
2026-05-01 17:36:55 +0200

    initial prisma integration

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import prisma from "../lib/prisma";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export async function loader() {
+  const playbackItems = await prisma.playbackItem.findMany();
+  return { playbackItems };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { playbackItems } = loaderData;
+  return <ul>{playbackItems.map((i) => <li key={i.id}>{i.title}</li>)}</ul>
 }
