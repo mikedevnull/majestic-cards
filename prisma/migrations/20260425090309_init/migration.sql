89e0ae7a79bed569184825b34d96b36afb931108
Michael Meier <michael-meier@bluesheep.de>
2026-05-03 19:25:45 +0200

    fix: add missing subtitle to prisma schema
    as this is all very initial, we're lazy and won't create a migration for this.

--- prisma/migrations/20260425090309_init/migration.sql
+++ prisma/migrations/20260425090309_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "PlaybackItem" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
+    "subtitle" TEXT,
     "backendUrl" TEXT,
     "imageUrl" TEXT,
     "triggerId" TEXT
